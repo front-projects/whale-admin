e4d5fbb3fef3150f68a394b8d2d6906a65c1a196
Front-projects <sasnykvlad2@gmail.com>
2024-07-25 12:47:36 +0300

    upd

--- components/Users/UsersTable.tsx
+++ components/Users/UsersTable.tsx
@@ -48,6 +48,21 @@ const UsersTable: React.FC = () => {
     fetchData();
   }, [page]);
 
+  useEffect(() => {
+    const disableDragEvents = (event: { preventDefault: () => void; }) => {
+      event.preventDefault();
+    };
+    // Встановлюємо обробники подій на документ
+    document.addEventListener('dragstart', disableDragEvents);
+    document.addEventListener('dragover', disableDragEvents);
+
+    // Очищуємо обробники подій при демонтажі компонента
+    return () => {
+      document.removeEventListener('dragstart', disableDragEvents);
+      document.removeEventListener('dragover', disableDragEvents);
+    };
+  }, []);
+
   const columns: GridColDef[] = [
     { field: "id", headerName: "ID", width: 90 },
     { field: "name", headerName: "Name", width: 150 },
