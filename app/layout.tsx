4eb54117238450a38ed3304e6513bcb581d13222
Front-projects <sasnykvlad2@gmail.com>
2024-08-02 14:21:18 +0300

    upd

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=no"
-        />
-      </Head>
       <body className={inter.className} suppressHydrationWarning={true}>
         {children}
       </body>
