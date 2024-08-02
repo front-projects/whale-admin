de040212d70f7f09186e0ef784ac8d74f9629e34
Front-projects <sasnykvlad2@gmail.com>
2024-08-02 14:19:18 +0300

    upd

--- components/Navigation/TitleForNav.tsx
+++ components/Navigation/TitleForNav.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation"
 export default function TitleForNav(){
     const pathname = usePathname();
    
-    const title = pathname.startsWith('/menu/users') ? 'USERS' : pathname.startsWith('/menu/deposits') ? 'DEPOSITS' : pathname.startsWith('/menu/top-users') ? 'TOP USERS' : "WITHDRAW"
+    const title = pathname.startsWith('/menu/users') ? 'USERS' : pathname.startsWith('/menu/deposits') ? 'DEPOSITS' : pathname.startsWith('/menu/top-users') ? 'TOP USERS' : pathname.startsWith('/menu/lottery') ? "LOTTERIES" : "WITHDRAW"
 
     return <div>{title}</div>
 }
