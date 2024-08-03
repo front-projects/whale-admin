c3c4e8ef0c5f367e73c1d34e2cfd1ab8701c53cd
Front-projects <sasnykvlad2@gmail.com>
2024-08-03 12:07:04 +0300

    upd

--- components/Users/LotteryStatus.tsx
+++ components/Users/LotteryStatus.tsx
@@ -5,7 +5,8 @@ import { MdOutlineCheck } from "react-icons/md";
 export default function LotteryStatus({ lottery }: { lottery: Lottery }) {
   return (
     <div
-      className={`max-md:text-[16px] max-sm:text-[12px] border-[1px] rounded-xl p-4 max-sm:p-2 shadow-xl flex flex-col flex-1 justify-center items-center ${lottery.investModelStatus === "BOUGHT" ? "border-green-600" : ""}`}
+      className={`max-md:text-[16px] max-sm:text-[12px] border-[1px] rounded-xl p-4 bg-gray-600/10
+        max-sm:p-2 shadow-xl flex flex-col flex-1 justify-center items-center ${lottery.investModelStatus === "BOUGHT" ? "border-green-600 text-green-600" : ""}`}
     >
       <div>{lottery.naming}</div>
       {lottery.investModelStatus === "BOUGHT" ? (
